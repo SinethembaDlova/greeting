b4473e3513daa65827489d348b9ee2b09670d3c7
SinethembaDlova <dSinethemba@gmail.com>
2017-03-06 18:03:44 +0200

    changes I did today

--- greet.js
+++ greet.js
@@ -1,10 +1,33 @@
-var showText = document.getElementById('takename');
-var displayText = document.getElementById('greetname');
+var takeText = document.querySelector('#takename');
+var radioButtons = document.getElementsByName('language');
+var greetmeButton = document.querySelector('#gbutton');
+var displayText = document.querySelector('#greetname');
+
 
 var greet = function()
 {
-  showText.value;
-  displayText.innerHTML = "Hello, " + showText.value;
-  return getText;
-}
-greet();
+  takeText.value;
+  console.log(radioButtons[0]);
+
+  for(i = 0; i < radioButtons.length; i++)
+  {
+    if (radioButtons.checked == radioButtons[i])
+    {
+      displayText.innerHTML = "Dumela, " + takeText.value;
+    }
+
+    else if (radioButtons.checked == radioButtons[1])
+    {
+      displayText.innerHTML = "Hello, " + takeText.value;
+    }
+
+    else if (radioButtons.checked == radioButtons[2])
+    {
+      displayText.innerHTML = "Molo, " + takeText.value;
+    }
+  }
+
+  return takeText.value;
+};
+
+greetmeButton.addEventListener('click', greet);
